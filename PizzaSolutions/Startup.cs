695b19aeb13846cd85c21ceb1aa2bfc0f161b658
Chris Goodrow <goodrow.chris4@gmail.com>
2018-02-27 12:34:28 -0500

    Add project files.

---
+++ PizzaSolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PizzaSolutions.Startup))]
+namespace PizzaSolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
