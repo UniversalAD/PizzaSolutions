37c3969102867c89fa1d6fa1dea5a33afe579fb6
Chris Goodrow <goodrow.chris4@gmail.com>
2018-04-26 13:54:16 -0500

    add more func to the products page

--- PizzaSolutions/Scripts/products.js
+++ PizzaSolutions/Scripts/products.js
@@ -2,16 +2,38 @@
 
     $(".box > a").click(function () {
         var item = $(this).data("target");
-        console.log(item);
 
         if ($(".slide-items").hasClass("slide-open")) {
-            $(".slide-items").removeClass("slide-open");            
+            $(".slide-items").removeClass("slide-open");
             $(item).addClass("slide-open")
-            console.log("has");
         }
         else {
-            $(item).addClass("slide-open");           
-            console.log("not");
+            $(item).addClass("slide-open");
         }
+    });
+    $(".box").click(function () {
+        var src = $(this).find("img").attr("src");
+        var imgId = $(this).find("img").attr("id");
+        console.log(src);
+        console.log(imgId);
+
+        if (imgId == "dominos")
+        {
+            $(".product-logo").attr("src", "/Content/Images/Logos/dominos-logo.png");
+            console.log("equals dominos");
+        }
+
+        else if (imgId == "pizza-hut")
+        {
+            $(".product-logo").attr("src", "/Content/Images/Logos/pizz-hut.png");
+            console.log("equals hut");
+        }
+
+        $(".ModalProductsImage").attr("src", src);
+    });
+
+    $(".color-pick").click(function () {
+        var id = $(this).attr("id");
+        console.log(id);
     });
 });
