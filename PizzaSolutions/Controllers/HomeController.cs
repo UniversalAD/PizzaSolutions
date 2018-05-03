dd961fd0dcf3e9e98aebb2af53b9a6f44849e230
Chris Goodrow <goodrow.chris4@gmail.com>
2018-05-03 12:54:24 -0500

    added cc emails

--- PizzaSolutions/Controllers/HomeController.cs
+++ PizzaSolutions/Controllers/HomeController.cs
@@ -241,8 +241,8 @@ namespace PizzaSolutions.Controllers
             {
                 var body = "Name: {0} <br /> Email: {1} <br /> Phone: {2} <br /> Company Name: {3} <br /> Website/Facebook: {4} <br /> Product: {5} <br /> Product Color: {6} <br /> Logo Color: {7}";
                 var message = new SendGridMessage();
-                //message.AddTo("partnerrelations@universalad.com");
-                //message.AddTo("connerg@universalad.com");  // replace with valid value 
+                message.AddTo("partnerrelations@universalad.com");
+                message.AddTo("connerg@universalad.com");  // replace with valid value 
                 message.AddTo("chrisg@universalad.com");
                 message.From = new MailAddress("partnerrelations@universalad.com");  // replace with valid value
                 message.Subject = "Product Selection Form";
