5eb4e66744f49a65ea6cfaa29d3e2c315de73d78
Chris Goodrow <goodrow.chris4@gmail.com>
2018-05-04 10:02:21 -0500

    started splash page and fixed slick issue

--- PizzaSolutions/Controllers/HomeController.cs
+++ PizzaSolutions/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace PizzaSolutions.Controllers
     {
         public bool EmailConfirmation;
 
+        public ActionResult Splash()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
         public ActionResult Index()
         {
             return View();
