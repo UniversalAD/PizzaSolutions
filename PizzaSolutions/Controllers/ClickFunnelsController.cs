fbbc0fd698cde5e8c7c6161f4a9f38f9198bcd11
Chris Goodrow <goodrow.chris4@gmail.com>
2018-05-07 10:31:59 -0500

    5/7 - 8:30 - 10:30
    finished splashpage and started funnel page.  Form classes are created
    just not linked to controller for submission.

---
+++ PizzaSolutions/Controllers/ClickFunnelsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaSolutions.Controllers
+{
+    public class ClickFunnelsController : Controller
+    {
+        // GET: ClickFunnels
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PizzaSolutions/Models/FreeMagnets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaSolutions.Models
+{
+    public class FreeMagnets
+    {
+        public string FirstLastName { get; set; }
+        public string Email { get; set; }
+        public string RestaurantName { get; set; }
+        public string JobTitle { get; set; }
+        public string CompanyWebsite { get; set; }
+        public string AvgNumDel { get; set; }
+    }
+}
