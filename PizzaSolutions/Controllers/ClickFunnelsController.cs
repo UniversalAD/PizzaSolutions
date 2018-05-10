0ed32507a7840d4b774bfacc3548b9e8fbe1ee2c
Chris Goodrow <goodrow.chris4@gmail.com>
2018-05-10 11:27:39 -0500

    started new site pages and finished clickfunnels page

--- PizzaSolutions/Controllers/ClickFunnelsController.cs
+++ PizzaSolutions/Controllers/ClickFunnelsController.cs
@@ -21,18 +21,18 @@ namespace PizzaSolutions.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult FreeMagnets (FreeMagnets model)
+        public ActionResult FreeMagnets(FreeMagnets model)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 var message = new SendGridMessage();
                 //message.AddTo("partnerrelations@universalad.com");
-                //message.AddTo("connerg@universalad.com");  // replace with valid value 
+                /*message.AddTo("connerg@universalad.com");*/  // replace with valid value 
                 message.AddTo("chrisg@universalad.com");
                 message.From = new MailAddress("partnerrelations@universalad.com");
                 message.Subject = "Click Funnel Free Magnets Sign Up";
 
-                var body = "<table><table style='width: 80%; margin: 0 auto; border: solid 1px #000'><th>Click Funnel Free Magnets Sign Up</th>" +
+                var body = "<table><table style='width: 100%; margin: 0 auto; border: solid 1px #000'><th>Click Funnel Free Magnets Sign Up</th>" +
                     "<tr>" +
                     "<td>Name {0}</td></tr>" +
                     "<tr><td>Email: {1}</td></tr>" +
@@ -71,16 +71,30 @@ namespace PizzaSolutions.Controllers
             message.From = new MailAddress("partnerrelations@universalad.com");
             message.Subject = "You Made a Great Decision!";
 
-            var body = "<table><table style='width: 80%; margin: 0 auto; border: solid 2px #d4d4d4;'>" +
-                "<th style='padding: 15px;'>You Made a Great Decision</th>" +
-                "<tr><td style='padding: 15px';>FirstLastName, Welcome to Pizza Solutions! We're so excited you've joined the team.</td></tr>" +
-                "<tr><td style='padding: 15px;'>Thank You for taking the time to fill out the form. We'll be getting your delivery magnets to you very soon!</td></tr>" +
-                "<tr><td style='padding: 15px';>If you have any questions, please call us @ 309-581-3073.</td></tr>" +
-                "<tr><td style='padding: 15px;'>Our Team is setting up you publication now and creating you ad copy/artwork.</td></tr>" +
-                "<tr><td style='padding: 15px;'>In event that you receive any phone calls from local businesses in you area inquiring about our program, do not worry... that's totally normal and means that they're interested in partnering with you and your pizzeria. Simply confirm your participation and we'll take it from there/<td></tr>" +
-                "<tr><td style='padding: 15px;>Thank you again, <br /> ~ Pizza Solutions</td></tr>" +
-                "<tr><td><img src='' style='max-width: 150px'></td></tr></table>";
-            message.Html = String.Format(body);
+            if (FirstLastName == null)
+            {
+                var body = "<table><table style='width: 80%; margin: 0 auto; border: solid 2px #d4d4d4;'>" +
+               "<th style='padding: 15px; font-size: 26px; font-weight: bold;'>You Made a Great Decision</th>" +
+               "<tr><td style='padding: 5px';>Welcome to Pizza Solutions! We're so excited you've joined the team.</td></tr>" +
+               "<tr><td style='padding: 5px;'>Thank You for taking the time to fill out the form. We'll be getting your delivery magnets to you very soon!</td></tr>" +
+               "<tr><td style='padding: 5px';>If you have any questions, please call us @ 309-581-3073.</td></tr>" +
+               "<tr><td style='padding: 5px;'>Our Team is setting up you publication now and creating you ad copy/artwork.</td></tr>" +
+               "<tr><td style='padding: 5px;'>In event that you receive any phone calls from local businesses in you area inquiring about our program, do not worry... that's totally normal and means that they're interested in partnering with you and your pizzeria. Simply confirm your participation and we'll take it from there/<td></tr>" +
+               "<tr><td style='padding: 5px;'>Thank you again, <br /> ~ Pizza Solutions</td></tr>" + "<tr><td style='padding: 5px;'><img src='https://lh6.googleusercontent.com/Sx_yWq3GY_rysjia0FeFd5lTPLjS0aTdZgzpEQAd6MxwUS74ZRievFgjxBh455OWesePjLJCFxaK8zLGg0vM=w1920-h925' style='max-width: '></td></tr></table></table>";
+                message.Html = String.Format(body);
+            }
+            else
+            {
+                var body = "<table><table style='width: 80%; margin: 0 auto; border: solid 2px #d4d4d4;'>" +
+               "<th style='padding: 15px; font-size: 26px; font-weight: bold;'>You Made a Great Decision</th>" +
+               "<tr><td style='padding: 5px';>{0}, Welcome to Pizza Solutions! We're so excited you've joined the team.</td></tr>" +
+               "<tr><td style='padding: 5px;'>Thank You for taking the time to fill out the form. We'll be getting your delivery magnets to you very soon!</td></tr>" +
+               "<tr><td style='padding: 5px';>If you have any questions, please call us @ 309-581-3073.</td></tr>" +
+               "<tr><td style='padding: 5px;'>Our Team is setting up you publication now and creating you ad copy/artwork.</td></tr>" +
+               "<tr><td style='padding: 5px;'>In event that you receive any phone calls from local businesses in you area inquiring about our program, do not worry... that's totally normal and means that they're interested in partnering with you and your pizzeria. Simply confirm your participation and we'll take it from there/<td></tr>" +
+               "<tr><td style='padding: 5px;'>Thank you again, <br /> ~ Pizza Solutions</td></tr>" + "<tr><td style='padding: 5px;'><img src='https://lh6.googleusercontent.com/Sx_yWq3GY_rysjia0FeFd5lTPLjS0aTdZgzpEQAd6MxwUS74ZRievFgjxBh455OWesePjLJCFxaK8zLGg0vM=w1920-h925' style='max-width: '></td></tr></table></table>";
+                message.Html = String.Format(body, FirstLastName);
+            }
 
             //Azure credentials
 
